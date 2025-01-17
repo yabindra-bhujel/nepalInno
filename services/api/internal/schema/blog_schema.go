21bc95b452b29fc122e79528924d66ee58c6d3cd
yabindra-bhujel <yabindra2057@gmail.com>
2025-01-17 20:42:40 +0900

    [schama] add blog schama

---
+++ services/api/internal/schema/blog_schema.go
@@ -0,0 +1,37 @@
+package schama
+
+
+type BlogInput struct {
+	Title     string   `json:"title"`
+	Content   string   `json:"content"`
+	Tags      *[]string `json:"tags"`           // Tag names or IDs
+	Thumbnail *string   `json:"thumbnail_image"` // Thumbnail URL
+}
+
+
+type UserOutput struct {
+	ID    string `json:"id"`
+	Email string `json:"email"`
+	Name  string `json:"name"`
+	Image string `json:"image"`
+}
+
+
+type BlogOutput struct {
+	ID          string   `json:"id"`
+	Title       string   `json:"title"`
+	Content     string   `json:"content"`
+	Thumbnail   string   `json:"thumbnail"`
+	Tags        []string `json:"tags"`
+	IsPublished bool     `json:"is_published"`
+	CreatedAt   string   `json:"created_at"`
+	TimeToRead  int      `json:"time_to_read"`
+	TotalViews  int      `json:"total_views"`
+	AuthorID string `json:"author_id"`
+	User        UserOutput `json:"user"`
+}
+
+type BlogListOutput struct {
+	Blogs []BlogOutput `json:"blogs"`
+	User UserOutput `json:"user"`
+}
